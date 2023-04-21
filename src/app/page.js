51420574c308184b1c6c35e18ffe63cf0866d87c
Msantra97 <monojsantra8013@gmail.com>
2023-04-21 20:48:47 +0530

    nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 import { Inter } from 'next/font/google'
-// import  Nav  from './components/Nav.js'
+import  Nav  from './components/Nav.js'
 // import  Carousel  from './components/Carousel.js'
 // import  Footer  from './components/Footer.js'
 // import  Gallery  from './components/Gallery.js'
 // import  About  from './components/About.js'
 // import  Service  from './components/Service.js'
-import  Login from './components/Login.js'
+// import  Login from './components/Login.js'
 // import  Register from './components/Register.js'
 export default function Home() {
   return (
   
   //  <div>
-  //      <Nav/>
+      //  <Nav/>
   //      <Carousel/>
   //      <Gallery/>
   //      <About/>
@@ -21,8 +21,8 @@ export default function Home() {
   //  </div>
   <div>
         
-    
-         <Login/> 
+        <Nav/>
+         {/* <Login/>  */}
          {/* < Register/> */}
   </div>
   )
